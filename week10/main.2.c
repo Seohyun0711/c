1f3ccb7527e0847f775652d63633888da0fabc9b
Seohyun0711 <nave4142@naver.com>
2024-11-06 13:44:35 +0900

    week10.2-2

---
+++ week10/main.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define SIZE 5
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i, average;
+	int sum;
+	int grade[SIZE];
+	
+	sum=0;
+	for(i=0;i<SIZE;i++)
+	{
+		printf("enter the score :");
+		scanf("%d", &grade[i]);
+		sum +=grade[i];
+		
+	}
+	average=sum/SIZE;
+	printf("average:%d\n",average);
+	
+
+	return 0;
+}
